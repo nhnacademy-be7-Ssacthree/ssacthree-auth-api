061b07982a374facdf73f8df6cf584ebe41d266c
김희망 <gmlakd990514@naver.com>
2024-10-21 09:53:46 +0900

    init: init

---
+++ src/main/java/com/nhnacademy/ssacthree_auth_api/SsacthreeAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ssacthree_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacthreeAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsacthreeAuthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/ssacthree_auth_api/SsacthreeAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ssacthree_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsacthreeAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
