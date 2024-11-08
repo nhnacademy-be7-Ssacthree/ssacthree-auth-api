5895c12626eed5c8ca5391f99a9ce0106286b063
김희망 <gmlakd990514@naver.com>
2024-11-08 21:17:15 +0900

    feat: 탈퇴한 회원 아이디로 로그인 방지

--- src/main/java/com/nhnacademy/ssacthree_auth_api/service/CustomUserDetailsService.java
+++ src/main/java/com/nhnacademy/ssacthree_auth_api/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.nhnacademy.ssacthree_auth_api.service;
 
 import com.nhnacademy.ssacthree_auth_api.domain.CustomUserDetails;
 import com.nhnacademy.ssacthree_auth_api.domain.Member;
+import com.nhnacademy.ssacthree_auth_api.exception.WithdrawMemberException;
 import com.nhnacademy.ssacthree_auth_api.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,13 +17,15 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
 
-
     @Override
     public UserDetails loadUserByUsername(String memberLoginId) throws UsernameNotFoundException {
 
         Member memberData = memberRepository.findByMemberLoginId(memberLoginId);
 
-        if(memberData != null) {
+        if (memberData != null) {
+            if (memberData.getMemberStatus().equalsIgnoreCase("withdraw")) {
+                throw new WithdrawMemberException("탈퇴한 회원입니다.");
+            }
             return new CustomUserDetails(memberData);
         }
         return null;
