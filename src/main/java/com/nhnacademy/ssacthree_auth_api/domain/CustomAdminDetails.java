073094d77ee19e9b503ef73e88039fe379ed95e0
김희망 <gmlakd990514@naver.com>
2024-11-29 17:35:08 +0900

    refactor: 중복코드 제거 2

--- src/main/java/com/nhnacademy/ssacthree_auth_api/domain/CustomAdminDetails.java
+++ src/main/java/com/nhnacademy/ssacthree_auth_api/domain/CustomAdminDetails.java
@@ -33,23 +33,5 @@ public class CustomAdminDetails implements UserDetails {
         return admin.getAdminName();
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  
 }
